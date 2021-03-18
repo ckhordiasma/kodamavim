01a227306fd917152010a28874c98e4f4b8d3bb5
Christopher Kodama <ckhordiasma@gmail.com>
2021-03-18 09:13:36 -0400

    added code to install node

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ cp ./.vimrc ~/.
 if ! command -v node &> /dev/null
 then
         echo "Node was not found.. you need to install node for autocomplete to work"
+        curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+        sudo bash n lts
+        sudo npm install -g n
+        rm ./n
         exit
 fi
-
+echo "Please go into vim and run :PlugInstall"
